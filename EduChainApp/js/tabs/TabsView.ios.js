35d00f476468844b51a3dad569d9a6c7cfec753c
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-06-19 22:21:45 +0100

    Move TabsView.ios to /tabs/ dir

--- EduChainApp/js/tabs/TabsView.ios.js
+++ EduChainApp/js/tabs/TabsView.ios.js
@@ -11,9 +11,9 @@ import {
     TabBarIOS,
     Navigator
 } from 'react-native'
-import {Tab} from './reducers/navigation'
-import First from './First'
-import Second from './Second'
+import {Tab} from './../reducers/navigation'
+import First from './../First'
+import Second from './../Second'
 
 export default class TabsView extends Component {
     constructor(props: Object) {
@@ -41,35 +41,35 @@ export default class TabsView extends Component {
                     title="Home"
                     selected={this.state.selectedTab === 'home'}
                     onPress={this.onTabSelect.bind(this, 'home')}
-                    icon={require('./img/iconbeast-lite/png/home-7.png')}>
+                    icon={require('./../img/iconbeast-lite/png/home-7.png')}>
                     <First navigator={{}} />
                 </TabBarIOS.Item>
                 <TabBarIOS.Item
                     title="Notifications"
                     selected={this.state.selectedTab === 'notifications'}
                     onPress={this.onTabSelect.bind(this, 'notifications')}
-                    icon={require('./img/iconbeast-lite/png/bell-7.png')}>
+                    icon={require('./../img/iconbeast-lite/png/bell-7.png')}>
                     <Second navigator={{}} />
                 </TabBarIOS.Item>
                 <TabBarIOS.Item
                     title="My Tasks"
                     selected={this.state.selectedTab === 'tasks'}
                     onPress={this.onTabSelect.bind(this, 'tasks')}
-                    icon={require('./img/iconbeast-lite/png/file-list-tick-7.png')}>
+                    icon={require('./../img/iconbeast-lite/png/file-list-tick-7.png')}>
                     <Second navigator={{}} />
                 </TabBarIOS.Item>
                 <TabBarIOS.Item
                     title="My Team"
                     selected={this.state.selectedTab === 'team'}
                     onPress={this.onTabSelect.bind(this, 'team')}
-                    icon={require('./img/iconbeast-lite/png/woman-man-7.png')}>
+                    icon={require('./../img/iconbeast-lite/png/woman-man-7.png')}>
                     <Second navigator={{}} />
                 </TabBarIOS.Item>
                 <TabBarIOS.Item
                     title="Me"
                     selected={this.state.selectedTab === 'me'}
                     onPress={this.onTabSelect.bind(this, 'me')}
-                    icon={require('./img/iconbeast-lite/png/circle-user-7.png')}>
+                    icon={require('./../img/iconbeast-lite/png/circle-user-7.png')}>
                     <Second navigator={{}} />
                 </TabBarIOS.Item>
             </TabBarIOS>
