d9491c9e8e014b40c30ff8d3cd022aa800b2e609
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-06-19 22:17:52 +0100

    Adds placeholder for #3

---
+++ EduChainApp/js/tabs/TabsView.android.js
@@ -0,0 +1,16 @@
+/**
+ * Created by BK on 19/06/16.
+ *
+ * @flow
+ */
+
+'use strict';
+
+import React, {Component} from 'react'
+import {
+    Navigator
+} from 'react-native'
+
+export default class TabsView extends Component {
+    // TODO GH issue #3
+}
