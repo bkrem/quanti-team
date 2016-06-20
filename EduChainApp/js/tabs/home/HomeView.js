c7f452e1010533f5f78ecf9deffec21dcbc605e3
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-06-20 14:30:49 +0100

    Adds placeholder for HomeView

--- EduChainApp/js/tabs/home/HomeView.js
+++ EduChainApp/js/tabs/home/HomeView.js
@@ -5,3 +5,21 @@
  */
 
 'use strict';
+
+import React, {Component} from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+
+export default class HomeView extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Test Text
+                </Text>
+            </View>
+        );
+    }
+}
