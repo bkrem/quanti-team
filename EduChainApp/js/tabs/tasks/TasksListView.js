919f97592d16096f752de8ed2269d01f81322025
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-06-21 16:31:55 +0100

    Add TODO

--- EduChainApp/js/tabs/tasks/TasksListView.js
+++ EduChainApp/js/tabs/tasks/TasksListView.js
@@ -69,6 +69,7 @@ export default class TaskView extends Component {
         );
     }
 
+    // TODO GH #6; add searchBar header
     render() {
         if (!this.state.loaded)
         return this.renderLoadingView();
