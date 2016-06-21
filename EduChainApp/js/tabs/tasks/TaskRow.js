cdcca51e9a1a23765f53e745f38b3bbc1335bb5c
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-06-21 16:40:07 +0100

    Turn each row element into a TouchableHighlight

--- EduChainApp/js/tabs/tasks/TaskRow.js
+++ EduChainApp/js/tabs/tasks/TaskRow.js
@@ -10,6 +10,7 @@
  import {
      Text,
      View,
+     TouchableHighlight,
      StyleSheet
  } from 'react-native';
 
@@ -27,16 +28,18 @@
 
      render() {
          return (
-             <View style={styles.container}>
-                 <View style={styles.leftColumn}>
-                     <Text style={styles.title}>{this.props.row.title}</Text>
-                     <Text style={styles.desc}>{this.props.row.desc}</Text>
+             <TouchableHighlight>
+                 <View style={styles.container}>
+                     <View style={styles.leftColumn}>
+                         <Text style={styles.title}>{this.props.row.title}</Text>
+                         <Text style={styles.desc}>{this.props.row.desc}</Text>
+                     </View>
+                     <View style={styles.rightColumn}>
+                         <Text style={styles.reward}>{this.props.row.reward}</Text>
+                         <Text style={styles.complete}>{this.props.row.complete}</Text>
+                     </View>
                  </View>
-                 <View style={styles.rightColumn}>
-                     <Text style={styles.reward}>{this.props.row.reward}</Text>
-                     <Text style={styles.complete}>{this.props.row.complete}</Text>
-                 </View>
-             </View>
+             </TouchableHighlight>
          );
      }
  }
