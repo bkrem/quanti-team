77e15b3304e9d63daafe83c3ce4be50852b5b17c
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-06-24 21:18:22 +0100

    Adds `contentWrapper` object to GlobalStyles for universal view padding

--- EduChainApp/js/common/GlobalStyles.js
+++ EduChainApp/js/common/GlobalStyles.js
@@ -6,10 +6,14 @@
  'use strict';
 
  const GlobalStyles = {
+     contentWrapper: {
+         paddingLeft: 5,
+         paddingRight: 5,
+     },
      sectionHeader: {
          fontSize: 20,
          marginTop: 10,
-     }
+     },
  }
 
  module.exports = GlobalStyles;
