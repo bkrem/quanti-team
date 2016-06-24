81d9268862dda805dec03b2bc60f2d16409e28a7
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-06-24 20:44:59 +0100

    Adds common `GlobalStyles` object

---
+++ EduChainApp/js/common/GlobalStyles.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @flow
+ */
+
+ 'use strict';
+
+ const GlobalStyles = {
+     sectionHeader: {
+         fontSize: 20,
+         marginTop: 10,
+     }
+ }
+
+ module.exports = GlobalStyles;
