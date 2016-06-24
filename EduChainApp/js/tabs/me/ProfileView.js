5ec37cb8d74e05727264fff984d6f46c35b6d4a1
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-06-24 21:19:27 +0100

    Adds basics for bio section

--- EduChainApp/js/tabs/me/ProfileView.js
+++ EduChainApp/js/tabs/me/ProfileView.js
@@ -9,10 +9,14 @@
  import React, { Component } from 'react';
  import {
      View,
+     StyleSheet,
+     Text,
  } from 'react-native'
+ import GlobalStyles from '../../common/GlobalStyles'
  import HeaderIOS from '../../common/Header'
  import ProfileSummary from './ProfileSummary'
 
+// TODO How to avoid declaring types twice, once here in top-lvl, once at component lvl?
  export default class ProfileView extends Component {
      render() {
          return (
@@ -25,8 +29,29 @@
                          icon: "ios-settings"
                      }}
                  />
-             <ProfileSummary username="@bkrem" name="Ben Kremer" teamname="Team λαμδα" />
+             <View style={GlobalStyles.contentWrapper}>
+                    <ProfileSummary
+                        username="@bkrem"
+                        name="Ben Kremer"
+                        teamname="Team λαμδα"
+                    />
+                    <View ref="bioContainer" style={styles.bioContainer}>
+                        <Text ref="bioTitle" style={GlobalStyles.sectionHeader}>Bio</Text>
+                        <Text ref="bio" style={styles.bioContent}>
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                       </Text>
+                   </View>
+                </View>
              </View>
          );
      }
  }
+
+ const styles = StyleSheet.create({
+     bioContainer: {
+         flex: 1,
+     },
+     bioContent: {
+         justifyContent: 'center'
+     }
+ })
