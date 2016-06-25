c6ee37dd184f269633ef4c4696cccffdef361585
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-06-25 19:10:47 +0100

    Add `thumbnail` global styling

--- EduChainApp/js/common/GlobalStyles.js
+++ EduChainApp/js/common/GlobalStyles.js
@@ -14,6 +14,11 @@
          fontSize: 20,
          marginTop: 10,
      },
+     thumbnail: {
+         height: 45,
+         width: 45,
+         borderRadius: 22, // why dafuq is 22 circular??
+     },
  }
 
  module.exports = GlobalStyles;
