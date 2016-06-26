bece79690701c5517280d4fb9acc2883ce773573
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-06-26 22:53:53 +0100

    Closes #12; adds form component for higher-level CommentBox component

--- EduChainApp/js/comments/CommentForm.js
+++ EduChainApp/js/comments/CommentForm.js
@@ -1,7 +1,5 @@
 /**
  * Created by BK on 25/06/16.
- *
- * @flow
  */
 
  'use strict';
@@ -10,15 +8,50 @@
  import {
      View,
      StyleSheet,
+     TouchableHighlight,
      Text,
  } from 'react-native'
+ import GlobalStyles from '../common/GlobalStyles'
+ import Button from 'react-native-button'
+ import t from 'tcomb-form-native'
 
 export default class CommentForm extends Component {
     render() {
         return (
-            <View>
-                <Text>CommentForm</Text>
+            <View style={styles.container}>
+                <Form
+                    ref="form"
+                    type={Comment}
+                    options={options}
+                />
+                <Button
+                    containerStyle={GlobalStyles.buttonContainer}
+                    style={styles.button}
+                >
+                    Submit
+                </Button>
             </View>
         );
     }
 }
+
+const Form = t.form.Form;
+const Comment = t.struct({ // Yayyy Golang syntax in JSX + ANOTHER static type schema -__-
+    comment: t.String
+});
+
+const options = {
+    fields: {
+        comment: {
+            placeholder: 'Add comment...'
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 15,
+    },
+    button: {
+    }
+})
