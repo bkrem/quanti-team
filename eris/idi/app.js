1cb99a291f27e266fdbce19e31387421d9820884
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-07-02 20:06:42 +0100

    Add test files for smart contract configs

---
+++ eris/idi/app.js
@@ -0,0 +1,60 @@
+// requires
+var fs = require ('fs');
+var prompt = require('prompt');
+var erisC = require('eris-contracts');
+
+// NOTE. On Windows/OSX do not use localhost. find the
+// url of your chain with:
+// docker-machine ls
+// and find the docker machine name you are using (usually default or eris).
+// for example, if the URL returned by docker-machine is tcp://192.168.99.100:2376
+// then your erisdbURL should be http://192.168.99.100:1337/rpc
+var erisdbURL = "http://localhost:1337/rpc";
+
+// get the abi and deployed data squared away
+var contractData = require('./epm.json');
+var idisContractAddress = contractData["deployStorageK"];
+var idisAbi = JSON.parse(fs.readFileSync("./abi/" + idisContractAddress));
+
+// properly instantiate the contract objects manager using the erisdb URL
+// and the account data (which is a temporary hack)
+var accountData = require('./accounts.json');
+var contractsManager = erisC.newContractManagerDev(erisdbURL, accountData.simplechain_full_000);
+
+// properly instantiate the contract objects using the abi and address
+var idisContract = contractsManager.newContractFactory(idisAbi).at(idisContractAddress);
+
+// display the current value of idi's contract by calling
+// the `get` function of idi's contract
+function getValue(callback) {
+  idisContract.get(function(error, result){
+    if (error) { throw error }
+    console.log("Idi's number is:\t\t\t" + result.toNumber());
+    callback();
+  });
+}
+
+// prompt the user to change the value of idi's contract
+function changeValue() {
+  prompt.message = "What number should Idi make it?";
+  prompt.delimiter = "\t";
+  prompt.start();
+  prompt.get(['value'], function (error, result) {
+    if (error) { throw error }
+    setValue(result.value)
+  });
+}
+
+// using eris-contracts call the `set` function of idi's
+// contract using the value which was recieved from the
+// changeValue prompt
+function setValue(value) {
+  idisContract.set(value, function(error, result){
+    if (error) { throw error }
+    getValue(function(){});
+  });
+}
+
+// run
+getValue(changeValue);
+
---
+++ eris/idi/idi.sol
@@ -0,0 +1,11 @@
+contract IdisContractsFTW {
+  uint storedData;
+
+  function set(uint x) {
+    storedData = x;
+  }
+
+  function get() constant returns (uint retVal) {
+    return storedData;
+  }
+}
