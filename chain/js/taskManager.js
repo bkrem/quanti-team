de806396745b0477762c28e52a7962d7c30ef7db
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-07-18 19:06:45 +0100

    Implement RPC methods to match TaskManager contract

--- chain/js/taskManager.js
+++ chain/js/taskManager.js
@@ -16,7 +16,10 @@ var eris = require(__libs+'/eris/eris-wrapper');
 
     var log = logger.getLogger('eris.chain.taskManager');
 
-    var events = {NEW_MESSAGE: "newMessage"};
+    var EVENTS = {
+        NEW_MESSAGE: "newMessage",
+        ADD_TASK: "ADD_TASK"
+    };
 
     // Set up event emitter
     function ChainEventEmitter () {
@@ -31,35 +34,29 @@ var eris = require(__libs+'/eris/eris-wrapper');
     var epmJSON = require(__root+'/epm.json');
     var accounts = require(__root+'/accounts.json');
     var taskManagerAbi = JSON.parse(fs.readFileSync(__abi+'/TaskManager'));
-    var taskAbi = JSON.parse(fs.readFileSync(__abi+'/Task'));
+    // var taskAbi = JSON.parse(fs.readFileSync(__abi+'/Task'));
 
     // Instantiate connection
     var erisWrapper = new eris.NewWrapper(__settings.eris.chain.host, __settings.eris.chain.port, accounts.simplechain_full_000);
     // Create contract objects
-    var taskManager = erisWrapper.createContract(taskManagerAbi, epmJSON['TaskManager']);
-    var taskContract = erisWrapper.createContract(taskAbi, epmJSON['Task']);
+    var taskManagerContract = erisWrapper.createContract(taskManagerAbi, epmJSON['TaskManager']);
+    // var taskContract = erisWrapper.createContract(taskAbi, epmJSON['Task']);
 
-    /* TODO Event Registration */
-    taskManager.ActionEvent(
+    taskManagerContract.ActionEvent(
         function (error, eventSub) {
             if (error)
                 throw error;
         },
         function (error, event) {
             if (event) {
-                // console.log("***EVENT***\n", eris.hex2str(event.args.actionType));
-                chainEvents.emit(events.NEW_DEAL, eris.hex2str(event.args.actionType));
+                var eventString = eris.hex2str(event.args.actionType);
+
+                console.log("***EVENT RAW***\n", event.args);
+                console.log("***EVENT ACTIONTYPE HEX:***\n", event.args.actionType);
+                console.log("***EVENT ACTIONTYPE STRING:***\n", eventString);
+                chainEvents.emit(EVENTS.ADD_TASK, event.args);
             }
         });
-
-    /**
-     * The init function can be used to perform further configuration on contracts
-     * @param callback
-     */
-    var init = function (callback) {
-        // nothing to do here
-        callback(null);
-    };
 
     /**
      * Adds a single task to the chain
@@ -67,22 +64,44 @@ var eris = require(__libs+'/eris/eris-wrapper');
      * @param callback
      */
     var addTask = function (task, callback) {
-        taskManager.addTask(
-            eris.str2hex("001"),
+        taskManagerContract.addTask(
+            eris.str2hex("003"),
             eris.str2hex("TestTitle"),
             eris.str2hex("Test Description"),
             eris.str2hex("To Do"),
             eris.str2hex("0/?"),
             eris.str2hex("200"),
              function (error, result) {
-                 log.debug("Added new task: " + result);
+                 log.debug("Overwrite?: " + result);
                  callback(error);
         });
     };
 
-    module.exports = {
-        'init': init,
-        'addTask': addTask
+    var getTaskAtIndex = function (id, callback) {
+        taskManagerContract.getTaskAtIndex(eris.str2hex(id), function (error, data) {
+            log.debug(data);
+            return error ? callback(error) : callback(data);
+        });
     };
 
+    var getTaskListSize = function (callback) {
+        taskManagerContract.getTaskListSize(function (error, size) {
+            log.debug("TaskListSize: " + size);
+            return error ? callback(error) : callback(size);
+        });
+    };
+
+     var getTaskKeyAtIndex = function (idx, callback) {
+        taskManagerContract.getTaskKeyAtIndex(idx, function (error, key) {
+            log.debug(eris.hex2str(key));
+            return error ? callback(error) : callback(key);
+        });
+    };
+
+    module.exports = {
+        addTask: addTask,
+        getTaskAtIndex: getTaskAtIndex,
+        getTaskListSize: getTaskListSize,
+        getTaskKeyAtIndex: getTaskKeyAtIndex
+    };
 }());
