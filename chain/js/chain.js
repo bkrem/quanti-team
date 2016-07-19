4c633ef5c6467cb02b7c3073b8448628009eebce
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-07-19 23:56:58 +0100

    Adds chain module

---
+++ chain/js/chain.js
@@ -0,0 +1,10 @@
+
+
+/**
+ * The init function can be used to perform further configuration on contracts
+ * @param callback
+ */
+var init = function (callback) {
+    // nothing to do here
+    callback(null);
+};
