331959026a058977c2fdc65561eed14a1de16276
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-07-22 23:51:24 +0100

    Expands action types

--- EduChainApp/js/actions/types.js
+++ EduChainApp/js/actions/types.js
@@ -1,4 +1,5 @@
 /**
+ * Partial src: https://github.com/fbsamples/f8app/blob/master/js/actions/types.js
  *
  * @flow
  */
@@ -7,4 +8,12 @@
 import type {Task} from '../reducers/tasks';
 
 export type Action =
-    { type: 'TASKS_LOADED', tasks: Array<Task> }
+    { type: 'TASKS_LOADED', taskList: Array<Task> }
+  | { type: 'TASK_TEST', val: string }
+  ;
+
+
+export type PromiseAction = Promise<Action>;
+export type Dispatch = (action: Action | ThunkAction | PromiseAction | Array<Action>) => any;
+export type GetState = () => Object;
+export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
