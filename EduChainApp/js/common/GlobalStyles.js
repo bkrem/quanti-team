b557cbb6c8e98fc5ed0183a8c902561389e4e013
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-07-26 03:35:04 +0200

    Add backBtn styling, fix indentation

--- EduChainApp/js/common/GlobalStyles.js
+++ EduChainApp/js/common/GlobalStyles.js
@@ -7,33 +7,38 @@
 
 import {StyleSheet} from 'react-native';
 
- const GlobalStyles = {
-     contentWrapper: {
-         paddingLeft: 5,
-         paddingRight: 5,
-     },
-     sectionHeader: {
-         fontSize: 20,
-         marginTop: 10,
-     },
-     thumbnail: {
-         height: 45,
-         width: 45,
-         borderRadius: 22, // why dafuq is 22 circular??
-     },
-     buttonContainer: {
-         padding:10,
-         paddingTop: 11,
-         height:45,
-         overflow:'hidden',
-         borderRadius:4,
-         backgroundColor: '#ededed'
-     },
-     separator: {
-         flex: 1,
-         height: StyleSheet.hairlineWidth,
-         backgroundColor: '#8E8E8E',
-     },
- };
+const GlobalStyles = {
+    contentWrapper: {
+        paddingLeft: 5,
+        paddingRight: 5,
+    },
+    sectionHeader: {
+        fontSize: 20,
+        marginTop: 10,
+    },
+    thumbnail: {
+        height: 45,
+        width: 45,
+        borderRadius: 22, // why dafuq is 22 circular??
+    },
+    buttonContainer: {
+        padding:10,
+        paddingTop: 11,
+        height:45,
+        overflow:'hidden',
+        borderRadius:4,
+        backgroundColor: '#ededed'
+    },
+    separator: {
+        flex: 1,
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: '#8E8E8E',
+    },
+    backBtn: {
+        title: "Back",
+        layout: "icon",
+        icon: "ios-arrow-back",
+    }
+};
 
- module.exports = GlobalStyles;
+module.exports = GlobalStyles;
