34ae288b24a9ecacecc5f079c659fb283fe58ba6
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-08-04 04:04:09 +0100

    #52; Adds UserManager module

--- chain/js/userManager.js
+++ chain/js/userManager.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var util = require('util');
 var EventEmitter = require('events');
+var Async = require('async');
 
 var chainUtils = require(__js+'/util/chainUtils');
 var eris = require(__libs+'/eris/eris-wrapper');
@@ -32,6 +33,48 @@ var chainEvents = new ChainEventEmitter();
 
 
 /**
+ * _createUserFromContract - Initializes a user object from the given contract.
+ *
+ * @param  {type} contract - A Solidity contract passed down from the public accessor function.
+ * @param  {func} callback - A callback passed down from the public accessor function.
+ * @return {callback} - Returns an error & empty object if `err` is not `null`,
+ *                      returns `null` & a user object otherwise.
+ */
+function _createUserFromContract (contract, callback) {
+    var user = {};
+
+    /* TODO potential refactor to iterate all keys automatically with `taskKeys` */
+    Async.parallel({
+        id: function (callback) {
+                contract.id( eris.convertibleCallback(callback, [eris.hex2str]) );
+            },
+        title: function (callback) {
+            contract.username( eris.convertibleCallback(callback, [eris.hex2str]) );
+        },
+        desc: function (callback) {
+            contract.email( eris.convertibleCallback(callback, [eris.hex2str]) );
+        },
+        status: function (callback) {
+            contract.name( eris.convertibleCallback(callback, [eris.hex2str]) );
+        },
+        complete: function (callback) {
+            contract.score( eris.convertibleCallback(callback, [eris.hex2str]) );
+        },
+        reward: function (callback) {
+            contract.teamId( eris.convertibleCallback(callback, [eris.hex2str]) );
+        }
+    },
+    function (err, results) {
+        if (err)
+            return callback(err, user);
+        user = results;
+        user.address = contract.address;
+        log.debug("Compiled user object:\n", user);
+        return callback(null, user);
+    });
+}
+
+/**
  * addUser - description
  *
  * @param  {type} user     description
@@ -43,17 +86,18 @@ function addUser (user, callback) {
 
     userManagerContract.addUser(
         hexUser.id,
-        hexUser.handle,
+        hexUser.username,
+        hexUser.email,
         hexUser.name,
         hexUser.score,
         hexUser.teamId,
-        function (err, result) {
-            err ? log.error("addUser() -> Error: " + err.stack) : log.debug("Overwrite?: " + result);
-            callback(err, result);
+        hexUser.passwHash,
+        function (err, address) {
+            err ? log.error("addUser() -> Error: " + err.stack) : log.debug("User address: " + address);
+            callback(err, address);
         }
     );
 }
-
 
 /**
  * getUserListSize - description
@@ -68,7 +112,59 @@ function getUserListSize (callback) {
     });
 }
 
+
+/**
+ * getUserAddress - description
+ *
+ * @param  {type} username description
+ * @param  {type} callback description
+ * @return {type}          description
+ */
+function getUserAddress (username, callback) {
+    log.debug('getUserAddress() -> username: ' + username);
+    userManagerContract.getUserAddress(eris.str2hex(username), function (err, address) {
+        err ? log.error("getUserAddress() -> Error: " + err.stack) : log.debug("User address: " + address);
+        callback(err, address)
+    });
+}
+
+/**
+ * getUserAtAddress - description
+ *
+ * @param  {type} address  description
+ * @param  {type} callback description
+ * @return {type}          description
+ */
+function getUser (address, callback) {
+    log.debug("getUser: Passed address:\n" + address);
+    userContract.at(address, function (error, contract) {
+        if (error)
+            throw error;
+        _createUserFromContract(contract, callback);
+    });
+}
+
+
+/**
+ * linkToTask - description
+ *
+ * @param  {type} username   description
+ * @param  {type} taskAddr description
+ * @param  {type} callback description
+ * @return {type}          description
+ */
+function linkToTask (username, taskAddr, callback) {
+    log.debug("linkToTask() -> username passed: ", username);
+    userManagerContract.linkToTask(eris.str2hex(username), taskAddr, function (error, success) {
+        log.debug("linkToTask() -> address returned: ", success);
+        callback(error, success);
+    });
+}
+
 module.exports = {
     addUser: addUser,
-    getUserListSize: getUserListSize
+    getUserAddress: getUserAddress,
+    getUser: getUser,
+    getUserListSize: getUserListSize,
+    linkToTask: linkToTask
 };
