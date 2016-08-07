2e726dc9534634737e9665fc0b0234e66fcb5022
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-08-07 20:58:44 +0100

    Adds `auth` module + tests

---
+++ chain/js/auth.js
@@ -0,0 +1,39 @@
+var userManager = require(__js+'/userManager');
+var logger = require(__libs+'/eris/eris-logger');
+var log = logger.getLogger('eris.chain.auth');
+
+
+/**
+ * login - description
+ *
+ * @param  {type} username description
+ * @param  {type} password description
+ * @param  {type} callback description
+ * @return {type}          description
+ */
+function login (username, password, callback) {
+    log.debug("login() for: ", username);
+    var isValid = false;
+
+    // TODO HASH THE PASSWORD
+
+    // Retrieve the user contract address from the chain
+    userManager.getUserAddress(username, function (addrErr, address) {
+        if (addrErr)
+            return callback(addrErr, isValid);
+        // Create a user object if there was no address error
+        userManager.getUser(address, function (err, user) {
+            if (err)
+                return callback(err, isValid);
+            // Compare the passed and retrieved login details to set `isValid`
+            if (user.username === username && user.password === password) {
+                isValid = true;
+            }
+            return callback(null, isValid);
+        });
+    });
+}
+
+module.exports = {
+    login: login
+}
---
+++ chain/test/auth_test.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+var assert = require('chai').assert;
+var app = require('../app');
+var auth = require(__js+'/auth');
+var userManager = require(__js+'/userManager');
+
+var testAuthUser = {
+    id: 'test0',
+    username: 'test_authuser',
+    email: 'testuser@test.com',
+    name: 'test user',
+    score: '3.14',
+    teamId: 'testTeam0',
+    password: 'testpass'
+};
+
+describe('auth', function () {
+    this.timeout(3000);
+
+    describe('login()', function () {
+        it('takes a username and a password, returns an `isValid` boolean', function (done) {
+            // add the user we want to test auth against
+            userManager.addUser(testAuthUser, function (err, address) {
+                // run a login
+                auth.login(testAuthUser.username, testAuthUser.password, function (err, isValid) {
+                    assert.isNull(err);
+                    assert.strictEqual(isValid, true, 'testUser should be able to succesfully log in');
+                    done();
+                });
+            });
+        });
+    });
+
+});
