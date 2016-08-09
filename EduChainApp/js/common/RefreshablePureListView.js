573886ee5de5ddb18e314477eaf478e28ab2cda9
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-08-09 18:02:09 +0100

    Adds debug logging for #47

--- EduChainApp/js/common/RefreshablePureListView.js
+++ EduChainApp/js/common/RefreshablePureListView.js
@@ -1,5 +1,5 @@
 /**
- * This component simply extends the FB library component `PureListView` by
+ * This component extends the FB library component `PureListView` by
  * adding React Native's <RefreshControl> component and adding `onRefresh`
  * event hooks. This enables refresh-on-pull-down capabilities.
  *
@@ -8,9 +8,7 @@
 'use strict';
 
 import React from 'react';
-import {
-    RefreshControl
-} from 'react-native';
+import {RefreshControl} from 'react-native';
 import PureListView from '../lib/facebook/PureListView';
 
 type Props = {
@@ -27,18 +25,33 @@ export default class RefreshablePureListView extends React.Component {
 
     // FIXME GH issue #47
     componentWillMount() {
-        //this.props.onRefresh();
+        console.info("RefreshablePureListView will mount");
+    }
+
+    componentDidMount() {
+        console.info("RefreshablePureListView did mount");
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.info("RefreshablePureListView componentWillReceiveProps", nextProps);
+    }
+
+    componentWillUnmount() {
+        console.info("RefreshablePureListView unmount")
     }
 
     render() {
-        const refreshControl =
-            (<RefreshControl
-                refreshing={this.props.refreshing}
-                onRefresh={this.props.onRefresh}
-            />);
-
         return (
-            <PureListView {...this.props} refreshControl={refreshControl} />
+            <PureListView
+                {...this.props}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={this.props.refreshing}
+                        onRefresh={this.props.onRefresh}
+                    />
+                }
+            />
         );
     }
+
 }
