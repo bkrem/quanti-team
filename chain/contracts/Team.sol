9c6131644d93251d5c895f64ec41caba3db90400
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-08-13 18:00:44 +0100

    Fleshes out Team contract, adds constructor

--- chain/contracts/Team.sol
+++ chain/contracts/Team.sol
@@ -1,22 +1,28 @@
+import "SequenceList.sol";
+
 contract Team {
 
     bytes32 id;
     bytes32 name;
-    bytes32[10] members;
 
-    // Fixed array size means a single team can have
-    // a maximum of 50,000 task addresses (i.e. tasks)
-    // associated with their account, for now.
-    uint16 MAXTASKS = 50000;
-    address[50000] public taskAddresses; // address[50000] == 1MB storage
+    // SequenceList to manage the members of this team.
+    SequenceList members = new SequenceList();
 
-    // `taskAddresses` auto-inits to .length == MAXTASKS.
-    // Separate counter variable is needed to track the
-    // actual size of `taskAddresses`.
-    uint16 sizeTaskAddresses;
+    // SequenceList to track all task addresses associated
+    // to this Team contract.
+    SequenceList taskAddressList = new SequenceList();
 
     // Constructor
-    function Team() {
+    function Team(
+        bytes32 _id,
+        bytes32 _name,
+        bytes32 _founderUsername,
+        address _founderAddress)
+        {
+        id = _id;
+        name = _name;
 
+        members.insert(_founderUsername, _founderAddress);
     }
+
 }
